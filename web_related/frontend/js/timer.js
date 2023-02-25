95b0b176d9097a9ec6148275a19fa685806b9e08
AveticsRobotics <73038067+Junszz@users.noreply.github.com>
2023-02-26 00:24:56 +0800

    Added progress bar

---
+++ web_related/frontend/js/timer.js
@@ -0,0 +1,117 @@
+// To Do
+// 1. When start session button is pressed, send a req to server
+// 2. When end session button is pressed, req for token earned
+
+/** Stores the reference to the elapsed time interval*/
+var elapsedTimeIntervalRef;
+
+/** Stores the start time of timer */
+var startTime;
+
+/** Stores the details of elapsed time when paused */
+var elapsedTimeWhenPaused;
+
+var prog_bar = document.getElementById("progressBar");
+
+function startTimer() {
+    // Create a new data object
+    startTime = new Date();
+    var initTime = 0;
+    var timetotal = 900;
+
+    elapsedTimeIntervalRef = setInterval(() =>{
+        elapsedTimeText.innerText = timeAndDateHandling.getElapsedTime(startTime);
+        // console.log("Hello");
+        // progress(initTime, timetotal, prog_bar);
+        var progressBarWidth = initTime * $("progressBar").width() / timetotal;
+        prog_bar.width = progressBarWidth
+        initTime ++;
+
+    }, 1000);
+}
+
+function stopTimer() {
+    if (typeof elapsedTimeIntervalRef !== "undefined") {
+        clearInterval(elapsedTimeIntervalRef);
+        elapsedTimeIntervalRef = undefined;
+    }
+}   
+
+function start_stop_session() {
+    if (btn.value == 'Start') {
+        // Start session
+        console.log("Start session");
+        btn.value = 'End';
+
+        // Timer function
+        startTimer();
+    }
+    else {
+        console.log("End session");
+        btn.value = "Start";
+
+        stopTimer();
+    }
+
+}
+
+var timeAndDateHandling = {
+    /** Computes the elapsed time since the moment the function is called in the format mm:ss or hh:mm:ss
+     * @param {String} startTime - start time to compute the elapsed time since
+     * @returns {String} elapsed time in mm:ss format or hh:mm:ss format if elapsed hours are 0.
+     */
+    getElapsedTime: function (startTime) {
+
+        // Record end time
+        let endTime = new Date();
+
+        // Compute time difference in milliseconds
+        let timeDiff = endTime.getTime() - startTime.getTime();
+
+        // Convert time difference from milliseconds to seconds
+        timeDiff = timeDiff / 1000;
+
+        // Extract integer seconds that dont form a minute using %
+        let seconds = Math.floor(timeDiff % 60); //ignoring uncomplete seconds (floor)
+
+        // Pad seconds with a zero if neccessary
+        let secondsAsString = seconds < 10 ? "0" + seconds : seconds + "";
+
+        // Convert time difference from seconds to minutes using %
+        timeDiff = Math.floor(timeDiff / 60);
+
+        // Extract integer minutes that don't form an hour using %
+        let minutes = timeDiff % 60; //no need to floor possible incomplete minutes, becase they've been handled as seconds
+
+        // Pad minutes with a zero if neccessary
+        let minutesAsString = minutes < 10 ? "0" + minutes : minutes + "";
+
+        // Convert time difference from minutes to hours
+        timeDiff = Math.floor(timeDiff / 60);
+
+        // Extract integer hours that don't form a day using %
+        let hours = timeDiff % 24; //no need to floor possible incomplete hours, becase they've been handled as seconds
+
+        // Convert time difference from hours to days
+        timeDiff = Math.floor(timeDiff / 24);
+
+        // The rest of timeDiff is number of days
+        let days = timeDiff;
+
+        let totalHours = hours + (days * 24); // add days to hours
+        let totalHoursAsString = totalHours < 10 ? "0" + totalHours : totalHours + "";
+
+        if (totalHoursAsString === "00") {
+            return minutesAsString + ":" + secondsAsString;
+        } else {
+            return totalHoursAsString + ":" + minutesAsString + ":" + secondsAsString;
+        }
+    }
+}
+
+function progress(start_time, timetotal, $element) {
+    var progressBarWidth = start_time * $element.width() / timetotal;
+    $element.find('div').animate({ width: progressBarWidth }, 500).html(Math.floor(start_time/60) + ":"+ start_time%60);
+}
+
+
